c23eeabe76f0f96aada097db600b0c0a8facece1
ItsArsalanMD <MohammedArsalan204@gmail.com>
2024-08-23 12:15:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-
+import Header from './components/Header';
+import Home from './Pages/Home';
+import Coin from './Pages/Coin';
+import { createTheme } from '@mui/material/styles';
 function App() {
+  const theme = createTheme();
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="app">
+          <Header />
+          <Routes>
+            <Route path="/" Component={Home} />
+            <Route path="/coins/:id" Component={Coin} />
+          </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
